5131ff8deb2ee1a77151f971aa519ba3fc2cb5a0
Lucian Mihai <mihai.lucian.florin@gmail.com>
2022-12-30 06:14:12 -0500

    map store to items using uids or uuids

---
+++ db.py
@@ -0,0 +1,12 @@
+# mapping between the stores and items based on UID
+stores = {}
+items = {
+    1: {
+        "name": "CCNA",
+        "price": 500
+    },
+    2: {
+        "name": "Devnet",
+        "price": 600
+    }
+}
